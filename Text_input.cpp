451d839f0fb901695205c77150afa01cbdb6c011
stealavie <118667726+stealavie@users.noreply.github.com>
2023-11-24 16:04:10 +0700

    Update Text_input.cpp

--- Text_input.cpp
+++ Text_input.cpp
@@ -1,90 +1,61 @@
-#include"Text_input.h"
+#include"Text_input2.h"
 
+Text_input::Text_input(int x, int y, int w, int h, SDL_Color color, TTF_Font* font)
+        : rect{x, y, w, h}, color{color}, font{font}, textSurface{nullptr}, textTexture{nullptr}, text{""} 
+{}
 
-TextInput::TextInput(SDL_Renderer* renderer, const char* file_location,const int& size, SDL_Color color, int x, int y)
-        : renderer(renderer), color(color), x(x), y(y) 
-{
-    std::string input="";
-    SDL_StartTextInput();
-    font = TTF_OpenFont(file_location, size);
-    if (!font) {
-        std::cerr << "TTF_OpenFont failed: " << TTF_GetError() << std::endl;
-        return;
-    }
-    flag = false;
-}
-
-
- TextInput::~TextInput() 
-{
-        SDL_StopTextInput();
-}
-
-
- void TextInput::handleEvent(SDL_Event* e,SDL_Window* window) 
+ Text_input::~Text_input() 
  {
-    while(flag)
-    {
-    while(SDL_PollEvent(e))
-    {
-    if (e->type == SDL_TEXTINPUT||e->type==SDL_KEYDOWN) 
-    {
-        system("cls");
-        if(e->type==SDL_KEYDOWN&&e->key.keysym.sym==SDLK_BACKSPACE&&input.length()>0)
+        if (textSurface) 
         {
-            input=input.substr(0,input.length()-1);
+            SDL_FreeSurface(textSurface);
         }
-        else if(e->type==SDL_TEXTINPUT)
-        input += e->text.text;
-        std::cout<<input<<std::endl;
-    }
-    else if(e->type==SDL_MOUSEBUTTONDOWN)
-        flag=false;
-    }
-    SDL_UpdateWindowSurface(window);
-    }
+        if (textTexture) 
+        {
+            SDL_DestroyTexture(textTexture);
+        }
 }
 
-void TextInput::draw() 
+void Text_input::handleEvent(SDL_Event* e) 
 {
-    SDL_Surface* surface = TTF_RenderText_Solid(font, input.c_str(), color);
-    if (!surface) {
-        std::cerr << "TTF_RenderText_Solid failed: " << TTF_GetError() << std::endl;
-        return;
+    if (e->type == SDL_TEXTINPUT) 
+    {
+        text += e->text.text;
+        updateTexture();
     }
-
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (!texture) {
-        std::cerr << "SDL_CreateTextureFromSurface failed: " << SDL_GetError() << std::endl;
-        SDL_FreeSurface(surface);
-        return;
+    else if(e->type==SDL_KEYDOWN&&e->key.keysym.sym==SDLK_BACKSPACE&&text.length()>0)
+    {
+        text=text.substr(0,text.length()-1);
     }
-
-    SDL_Rect dest;
-    dest.x = x;
-    dest.y = y;
-    SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
-
-    if (SDL_RenderCopy(renderer, texture, NULL, &dest) != 0) {
-        std::cerr << "SDL_RenderCopy failed: " << SDL_GetError() << std::endl;
-    }
-
-    SDL_FreeSurface(surface);
-    SDL_DestroyTexture(texture);
 }
 
 
-std::string TextInput::Get_input()
+void Text_input::updateTexture() 
 {
-    return this->input;
+    if (textSurface) 
+    {
+        SDL_FreeSurface(textSurface);
+    }
+    if (textTexture) 
+    {
+        SDL_DestroyTexture(textTexture);
+    }
+    textSurface = TTF_RenderText_Solid(font, text.c_str(), color);
+    textTexture = SDL_CreateTextureFromSurface(SDL_GetRenderer(SDL_GetWindowFromID(1)), textSurface);
 }
 
-void TextInput::setFlag(bool flag) 
+void Text_input::render() 
+{
+    SDL_RenderCopy(SDL_GetRenderer(SDL_GetWindowFromID(1)), textTexture, NULL, &rect);
+}
+
+
+void Text_input::setClicked(bool flag)
 {
     this->flag=flag;
 }
 
-bool TextInput::getFlag()
+bool Text_input::isClicked()
 {
     return this->flag;
 }
