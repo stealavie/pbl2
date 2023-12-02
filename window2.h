630e2e6413b3129cee4d9b36f6d95f1a653cd678
stealavie <118667726+stealavie@users.noreply.github.com>
2023-12-02 21:02:51 +0700

    Update window2.h

--- window2.h
+++ window2.h
@@ -1,19 +1,19 @@
 #pragma once
-#include"TextureManagement.h"
+
 #include"Button.h"
-#include<iostream>
-#include"Text_input2.h"
-#include"User.h"
-class Window
+#include"TextBox.h"
+
+class Window 
 {
-    public:
-    static SDL_Window* window;
+public:
     static SDL_Renderer* renderer;
+    static SDL_Window* window;
+    static TTF_Font* font;
+    static SDL_Color color;
     SDL_Texture* background;
-    virtual void Enter()=0;
-    virtual void Update()=0;
-    virtual void Exit()=0;
-    bool running=false;
+    virtual void Enter() = 0;
+    virtual void Update() = 0;
+    bool running = false;
     void shutdown();
     Window();
     virtual ~Window();
@@ -21,52 +21,49 @@ class Window
     static Window* loginWindow;
     static Window* currentWindow;
     static Window* projectWindow;
-    static TTF_Font* font;
-    static SDL_Color color;
-
 };
 
 class MainWindow : public Window
 {
-    private:
-    Button* Login_button,*Explore_button;
-    public:
+private:
+    Button* Login_button, * Explore_button;
+public:
     void Enter();
     void Update();
-    void Exit();
     MainWindow();
-    virtual ~MainWindow(); 
+    virtual ~MainWindow();
 };
 
 
 class LoginWindow : public Window
 {
-    private:
-    Button* Return_button,*Password,*Username,*Login;
-    Text_input* Username_input,*Password_input;
-    std::string u_Username,u_Password;
-    Trie* obj;
-    public:
+private:
+    Button* Return_button, * Password, * Username, * Login;
+    std::string u_Username, u_Password;
+    TextBox* b_username, * b_password;
+public:
     void Enter();
     void Update();
-    void Exit();
+
     LoginWindow();
-    virtual ~LoginWindow(); 
+    virtual ~LoginWindow();
 };
 
 
 class  ProjectWindow : public Window
 {
-    private:
-    Button* return_main,*Trending_button,*Nearly_button,
-            *JustLaunch_button,*Everything_button;
-    public:
+private:
+    Button* return_main, * Trending_button, * Nearly_button,
+        * JustLaunch_button, * Everything_button;
+public:
     void Enter();
     void Update();
-    void Exit();
+
     ProjectWindow();
-    
+
     virtual ~ProjectWindow();
 };
+
+
 
 
