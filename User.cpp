9ab156ef8726061745a0bdb81700fc5492f3a78c
curdzmusic <149883098+curdzmusic@users.noreply.github.com>
2023-12-09 11:32:12 +0700

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -9,50 +9,98 @@ User::User()
 
 void User::Load_data() 
 {
-    std::ifstream ip("D:/New folder/Data_test.csv");
-    if(!ip.is_open()) 
+    std::ifstream ip("C:/Users/USER/Desktop/pbl2/Data_test.csv");
+    if (!ip.is_open()) 
     {
-        std::cout<<"ERROR!"<<std::endl;
+        std::cout << "ERROR! Unable to open the file." << std::endl;
         return;
-    }else
+    }
+    else
     {
-        std::cout<<"open success!"<<std::endl;
-        while(std::getline(ip,this->Username,',')&&
-        std::getline(ip,this->Password,'\n'))
+        std::cout << "File opened successfully!" << std::endl;
+        while (std::getline(ip, this->Username, ',') &&
+               std::getline(ip, this->Password, '\n'))
         {
-        Insert(this->Username,this->Password);
-        this->Username.clear();
-        this->Password.clear();
-        }   
-    ip.close();
+            if (!this->Username.empty() && !this->Password.empty())
+            {
+                Insert(this->Username, this->Password);
+            }
+            this->Username.clear();
+            this->Password.clear();
+        }
+        ip.close();
+
+        // // In ra toàn bộ dữ liệu sau khi đọc từ file
+        // Trie_node* root = this->getRoot();
+        // std::vector<std::pair<std::string, info*>> allUserData;
+        // this->traverseTrie(root, allUserData, "");
+
+        // for (const auto& userData : allUserData) {
+        //     std::cout << "Username: " << userData.first << ", Password: " << userData.second->password << std::endl;
+        // }
     }
 }
+
+
+// void User::update_data() 
+// {
+//     ofstream ip;
+//     ip.open("C:/Users/USER/Desktop/pbl2/Data_test.csv",ofstream::out | ofstream::trunc);
+//     ip.close();
+//     ip.open("C:/Users/USER/Desktop/pbl2/Data_test.csv");
+
+
+//     vector<pair<string, info*>> username;
+//     Trie::traverseTrie(this->root,username,"");
+
+//     for (auto &user : username) 
+//     {
+//         ip << user.first<<","<<user.second->password<<"\n";
+//     }
+
+//     ip.close();
+//     std::cout<<"update success"<<std::endl;
+    
+// }
+
 
 void User::update_data() 
 {
     ofstream ip;
-    ip.open("D:/New folder/Data_test.csv",ofstream::out | ofstream::trunc);
-    ip.close();
-    ip.open("D:/New folder/Data_test.csv");
-
-    vector<string> username;
-    Trie::traverseTrie(this->root,username,"");
-
-    for (auto &user : username) 
+    ip.open("C:/Users/USER/Desktop/pbl2/Data_test.csv", ofstream::out | ofstream::trunc);
+    if (!ip.is_open()) 
     {
-        Trie_node* node = this->root;
-        for (char c : user) 
-        {
-        node = node->getChild(c);
-        }
-        ip << user<<","<<node->getInfo()<<"\n";
-        
+        std::cout << "ERROR! Unable to open the file for writing." << std::endl;
+        return;
     }
 
+    // Vector để lưu dữ liệu từ Trie
+    vector<pair<string, info*>> username;
+    // Duyệt cây Trie và lưu dữ liệu vào vector
+    Trie::traverseTrie(this->root, username, "");
+
+    // In ra số lượng phần tử trong vector để kiểm tra
+    std::cout << "Number of elements in vector: " << username.size() << std::endl;
+
+    // Ghi dữ liệu từ vector vào file
+    for (const auto &user : username) 
+    {
+        ip << user.first << "," << user.second->password << "\n";
+    }
+
+    // Đóng file và kiểm tra
     ip.close();
-    std::cout<<"update success"<<std::endl;
-    
+    if (ip.is_open()) 
+    {
+        std::cout << "ERROR! Unable to close the file." << std::endl;
+        return;
+    }
+    std::cout << "File closed successfully!" << std::endl;
+    std::cout << "Update success" << std::endl;
 }
+
+
+
 
 void User::Insert(const string& Username, const string& Password)
 {
@@ -67,5 +115,17 @@ void User::Insert(const string& Username, const string& Password)
         node = node->getChild(c);
     }
     node->setEndOfWord(true);
-    node->setInfo(Password);
+    // node->setInfo(Password);
+
+    // Create a new info object and set its password
+    info* Info = new info;
+    Info->password = Password;
+    
+    // Pass the info object to setInfo()
+    node->setInfo(Info);
+}
+
+void User::Print()
+{
+    std::cout << "Username: " << Username << ", Password: " << Password << std::endl;
 }
