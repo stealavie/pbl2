c5dc0b26ade032a80250e1a6bd79ccae807837fe
stealavie <118667726+stealavie@users.noreply.github.com>
2024-01-05 20:26:55 +0700

    Update Projects.cpp

--- Projects.cpp
+++ Projects.cpp
@@ -1,20 +1,9 @@
 #include "Projects.h"
 
-Project::Project(std::string name, std::string description, std::string imagePath, std::string pledged)
+Project::Project(std::string name, std::string description, std::string imagePath)
 {
     this->name = name;
     this->description = description;
     this->imagePath = imagePath;
-    this->pledged = pledged;
     this->next = nullptr;
 }
-
-/*Project* Project::operator = (Project* p)
-{
-    this->name = p->name;
-    this->description = p->description;
-    this->imagePath = p->imagePath;
-    this->pledged = p->pledged;
-    this->next = p->next;
-    return (this);
-}*/
